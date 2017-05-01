474ed746f3870c19a86fa2f9697d03729aad6ead
mitch.thompson <thomitchell@gmail.com>
2017-05-01 22:48:17 +0000

    Adds 5/1 status report

--- prod/storage/framework/views/d033c49753f32649f8a47647495fab395ee00d58.php
+++ prod/storage/framework/views/d033c49753f32649f8a47647495fab395ee00d58.php
@@ -115,8 +115,6 @@
 <!-- ChartJS-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>   
 <script type="text/javascript" src="js/charts-demo-data.js"></script>
-
-  
   
 
 <?php $__env->stopSection(); ?>
