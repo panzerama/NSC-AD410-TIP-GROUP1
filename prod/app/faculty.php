e76ad0d8d1ed8069e9ebe17a25bebe6a30415381
JD Panzer <jason.drew.panzer@gmail.com>
2017-05-02 18:38:16 +0000

    many to many relationship example

--- prod/app/faculty.php
+++ prod/app/faculty.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class faculty extends Model
 {
-    //
+    //defining a many to many relation between faculty member and tip report models
+    public function tips() {
+        /* general layout
+        belongsToMany(<model to join name>, <joining table name>, <current model id>, 
+         <model being joined id>);
+        http://laraveldaily.com/pivot-tables-and-many-to-many-relationships/ */
+        return $this->belongsToMany('App\tip', 'faculty_tips', 'faculty_id', 'tips_id');
+    }
 }
