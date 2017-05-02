5596fd685c623df3f7407a472b65af172aea11bb
JD Panzer <jason.drew.panzer@gmail.com>
2017-05-02 06:43:39 +0000

    status update

---
+++ prod/database/migrations/2017_04_29_065812_create_answers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('answer_id');
+            $table->integer('question_id')->unsigned();
+            $table->text('answer_text');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+        
+         Schema::table('answers', function (Blueprint $table) {
+            $table->foreign('question_id')
+                  ->references('question_id')
+                  ->on('questions')
+                  ->onDelete('cascade');
+         });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
---
+++ prod/database/migrations/2017_04_29_070122_create_tips_questions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipsQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tips_questions', function (Blueprint $table) {
+            $table->integer('tips_id')->unsigned();
+            $table->integer('question_id')->unsigned();
+            $table->text('question_answer')->nullable();
+            $table->timestamps();
+        });
+        
+        Schema::table('tips_questions', function (Blueprint $table) {
+            $table->foreign('tips_id')
+                  ->references('tips_id')
+                  ->on('tips')
+                  ->onDelete('cascade');
+            
+            $table->foreign('question_id')
+                  ->references('question_id')
+                  ->on('questions')
+                  ->onDelete('cascade');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tips_questions');
+    }
+}
