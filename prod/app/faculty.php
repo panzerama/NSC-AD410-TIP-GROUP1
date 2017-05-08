4368fa9efa90a07588f23b37e9f2e53e78683ed1
JD Panzer <jason.drew.panzer@gmail.com>
2017-05-08 20:59:55 +0000

    faculty model

--- prod/app/faculty.php
+++ prod/app/faculty.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class faculty extends Model
 {
+    
+    protected $fillable = [
+        'faculty_id',
+        'division_id',
+        'faculty_name',
+        'email',
+        'employee_type',
+        'is_admin',
+        'is_active'
+        ];
+        
     //defining a many to many relation between faculty member and tip report models
     public function tips() {
         /* general layout
@@ -13,6 +24,10 @@ class faculty extends Model
          <model being joined id>);
         http://laraveldaily.com/pivot-tables-and-many-to-many-relationships/ */
         return $this->belongsToMany('App\tip', 'faculty_tips', 'faculty_id', 'tips_id');
+    }
+    
+    public function faculty() {
+        return $this->hasOne('App\faculty');
     }
 }
 // A little test for the testing commit line.
