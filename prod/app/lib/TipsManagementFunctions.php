1fa3e5fc3b673d04e205e751130902af769c2032
JD Panzer <jason.drew.panzer@gmail.com>
2017-05-10 03:56:54 +0000

    edit tip questions UI's

---
+++ prod/app/lib/TipsManagementFunctions.php
@@ -0,0 +1,39 @@
+
+        function addbefore() {
+            
+            echo "The add new before function is called.";
+            exit;
+        }
+        function addafter() {
+            echo "The add new afterfunction is called.";
+            exit;
+        }
+        function moveup() {
+            echo "The moveup function is called.";
+            exit;
+        }
+        function movedown() {
+            echo "The movedown function is called.";
+            exit;
+        }
+        function modify() {
+            echo "The modify function is called.";
+            exit;
+        } 
+        function inactivate() {
+            echo "The inactivate function is called.";
+            exit;
+        }
+        function activate() {
+            echo "The activate function is called.";
+            exit;
+        }
+        
+        function cancel() {
+            echo "The cancel function is called.";
+            exit;
+        }
+        function submit() {
+            echo "The submit function is called.";
+            exit;
+        }
--- prod/resources/views/admin/tips-management-edit-impact-list.blade.php
+++ prod/resources/views/admin/tips-management-edit-impact-list.blade.php
@@ -1,14 +1,126 @@
+@extends('layouts.admin-app')
 <style>
-    .textarea{
-    font-size: 60px;
+    .scrollabletextbox {
+    height:55px;
+    width:800px;
+    font-family: Verdana, Tahoma, Arial, Helvetica, sans-serif;
+    font-size: 125%;
+    word-wrap: medium;
+    }
+
+    .button {
+    display: inline-block;
+    }    
+    
+    .buttonaddnewbefore{
+    width: 120px;
+    height: 40px;
+    border: 3px solid RebeccaPurple;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: Lavender;
+    text-align: center;
+    font-weight:bold;
     }
     
-</style>
+    .buttonaddnewafter{
+    width: 120px;
+    height: 40px;
+    border: 3px solid ForestGreen;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: Plum;
+    text-align: center;
+    font-weight:bold;
+    }
+    
+    .buttonmoveup{
+    width: 120px;
+    height: 40px;
+    border: 3px solid MidnightBlue;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: Aqua;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
+    .buttonmovedown{
+    width: 120px;
+    height: 40px;
+    border: 3px solid MidnightBlue;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: DeepSkyBlue;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
 
-<?php $__env->startSection('title', 'TIPS Management'); ?>
-<?php $__env->startSection('content'); ?>
+    .buttonmodify{
+    width: 120px;
+    height: 40px;
+    border: 3px solid DarkOrange;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: PeachPuff;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
+
+    .buttoninactivate{
+    width: 120px;
+    height: 40px;
+    border: 3px solid Red;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: MistyRose;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
+
+    .buttonactivate{
+    width: 120px;
+    height: 40px;
+    border: 3px solid ForestGreen;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: Lime;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
+
+    .buttoncancel{
+    width: 250px;
+    height: 40px;
+    border: 3px solid Red;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: Red;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
+
+    .buttonsubmit{
+    width: 250px;
+    height: 40px;
+    border: 3px solid Lime;
+    border-collapse: collapse;
+    padding: 6px;
+    background-color: Lime;
+    display: inline;
+    text-align: center;
+    font-weight:bold;
+    }
+
+</style>
+@section('title', 'TIPS Management')
+@section('content')
 <?php
-    set_include_path('lib/TipsManagementFunctions.php');
     $q_number = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
     $length_q_number = count($q_number);
     $q_text = array("What is your Division?","Course Prefix?","Course Number?","Quarter-Year?","What is the problem or lesson that you identified and will be discussing in this TIP? No topic is too big or too small. All are welcomed!","What is the course-level objective that this TIP best addresses?","Which of the college-wide Essential Learning Outcomes does your TIP most closely address?","Which of the following best describes the evidence you found for the problem?","Please describe more specifcally how you identified the problem. For example, Based on discussion posts, I realized that more than half of the class did not understand the prompt and was not demonstrating the kind of comprehension I was looking for.","Please select the change that best fits what you did to try to address the problem.","Specifically, what did you do to address the problem? For example, I broke the prompt down into two separate discussions so that it was clearer what the students should think about and write about in their posts.","Please select the evidence that best fits how you assessed the impact of the change you made.","Please describe more fully how you assessed the impact of the change you made. For example,  After I broke the prompt into two discussions, more students were able to write about the ideas thoroughly. This time it was about 75% of students. I might want to refine the prompts even further, but this was a good change.","What new opportunities did you consider as a result of identifying this problem and making this change?","What else would you like to share about the teaching improvement process you engaged in this quarter?","TIP data will be shared de-identified and in aggregate. TIPs are NOT an evaluation of your teaching. It is useful to campus-wide assessment and professional development to use specifics of individual TIPs.");
@@ -32,44 +144,74 @@
                       <h2>TIPS Management</h2>  
                     </div>
                     <div class="ibox-title">
-                      <h3>Question List<br> </h3>
+                      <h3>Answer Options - Impact List<br> </h3>
                     </div>
                 </div>    <!-- end ibox -->
             </div>    <!-- end col lg 12 -->
         </div>    <!-- end row -->
     </div>     <!-- wrapper -->
     <div class="panel panel-body" align="left">
-        <?php for($i = 0, $l = count($q_number); $i < $l; ++$i) {
-        $question = $q_text[$i];
+        <?php for($i = 0, $l = count($answer_impact_options); $i < $l; ++$i) {
+        $impact = $answer_impact_options[$i];
         ?>
             <form>
                 <!-- begin row -->
                 <div class="row">
                     <!-- col 1 -->
-                    <div class="col-lg-6">
-                    <textarea style="width:100%" class="text-area" name="q_text"><?php echo $question; ?></textarea>
+                    <div class="col-sm-6">
+                    <textarea class="scrollabletextbox" name="impact"><?php echo $impact; ?></textarea>
                     </div>
-                    <div class="col-lg-6">
-                        <p>
-                            <button type="button" class="btn btn-outline btn-default" name="addbefore" >Add New Before</button>
-                            <button type="button" class="btn btn-outline btn-primary" name="addafter">Add New After</button>
-                            <button type="button" class="btn btn-outline btn-success" name="moveup">Move Up</button>
-                            <button type="button" class="btn btn-outline btn-info" name="movedown">Move Down</button>
-                            <button type="button" class="btn btn-outline btn-warning" name="modify">Modify</button>
-                        <?php   if($q_is_active[$i] == 1){
-                        ?>
-                            <button type="button" class="btn btn-outline btn-danger" name="inactivate">Inactivate</button>
-                        <?php   } else{
-                        ?>        
-                            <button type="button" class="btn btn-outline btn-primary" name="activate">Activate</button>
-                        <?php   };
-                        ?>
-                        </p>
-                    </div>  <!-- end col lg 6 -->
+                    
+                    <!-- col 2 -->
+                    <div class="col-sm-1">
+                        <input type="submit" class="buttonaddnewbefore" name="addbefore" value="Add New Before" />
+                    </div>
+                    <!-- col 3 -->
+                    <div class="col-sm-1">
+                    <input type="submit" class="buttonaddnewafter" name="addafter" value="Add New After" />
+                    </div>
+                    <!-- col 4 -->
+                    <div class="col-sm-1">
+                    <input type="submit" class="buttonmoveup" name="moveup" value="Move Up" />
+                    </div>
+                    <!-- col 5 -->
+                    <div class="col-sm-1">
+                    <input type="submit" class="buttonmovedown" name="movedown" value="Move Down" />
+                    </div>
+                    <!-- col 6 -->
+                    <div class="col-sm-1">
+                    <input type="submit" class="buttonmodify" name="modify" value="Modify" />
+                                        </div>
+                    <!-- col 7 -->
+                    <div class="col-sm-1">
+                    <?php   if($q_is_active[$i] == 1){
+                    ?>        
+                    <input type="submit" class="buttoninactivate" name="inactivate" value="Inactivate" />
+                    <?php   } else{
+                    ?>        
+                    <input type="submit" class="buttonactivate" name="activate" value="Activate" />
+                    <?php   };
+                    ?>
+                    </div>
                 </div>   <!-- end row -->
                 <br>
         <?php }; ?>   <!-- end for loop -->
-             
+                <div class="row">
+                    <!-- col 1 -->
+                    <div class="col-sm-3">
+                    </div>
+                    <!-- col 2 -->
+                    <div class="col-sm-3">
+                    </div>
+                    <!-- col 3 -->
+                    <div class="col-sm-3">
+                        <input type="submit" class="buttoncancel" name="cancel" value="Cancel All Changes" />
+                    </div>
+                    <!-- col 4 -->
+                    <div class="col-sm-3">
+                        <input type="submit" class="buttonsubmit" name="submit" value="Submit All Changes" />
+                    </div>
+                </div>
                 <br>
         </form> 
     </div>      <!-- end panel body -->        
@@ -105,7 +247,43 @@
                 break;
             }
         }
+        
+        function addbefore() {
+            echo "The add new before function is called.";
+            exit;
+        }
+        function addafter() {
+            echo "The add new afterfunction is called.";
+            exit;
+        }
+        function moveup() {
+            echo "The moveup function is called.";
+            exit;
+        }
+        function movedown() {
+            echo "The movedown function is called.";
+            exit;
+        }
+        function modify() {
+            echo "The modify function is called.";
+            exit;
+        } 
+        function inactivate() {
+            echo "The inactivate function is called.";
+            exit;
+        }
+        function activate() {
+            echo "The activate function is called.";
+            exit;
+        }
+        
+        function cancel() {
+            echo "The cancel function is called.";
+            exit;
+        }
+        function submit() {
+            echo "The submit function is called.";
+            exit;
+        }
     ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin-app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+@endsection
