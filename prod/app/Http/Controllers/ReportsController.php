5bf7bdeb3366500f97cc36d47166beb94e43006e
Ilsa Coleman <icoleman00@gmail.com>
2017-05-13 19:56:41 +0000

    add reports controller

---
+++ prod/app/Http/Controllers/ReportsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ReportsController extends Controller
+{
+    //admin
+    public function index()
+    {
+        return view('reports/index');
+    }
+    
+}
