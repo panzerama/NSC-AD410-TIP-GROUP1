4a0eb6603d7793b1a9c1cb808001a5788c893e62
Ilsa Coleman <icoleman00@gmail.com>
2017-05-16 04:23:30 +0000

    few small changes to admin UI + status report

--- prod/resources/views/reports/table.blade.php
+++ prod/resources/views/reports/table.blade.php
@@ -42,7 +42,7 @@
                     </div>
                     <div class="ibox-content">
 
-                        <div class="table-responsive">
+                         <div class="table-responsive">
                     <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="html5buttons">
                         <div class="dt-buttons btn-group">
                         <a class="btn btn-default buttons-copy buttons-html5" tabindex="0" aria-controls="DataTables_Table_0" href="#"><span>Copy</span></a>
@@ -63,13 +63,15 @@
                     <thead>
                     <tr role="row">
                     <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Division: activate to sort column ascending" style="width: 289px;">Division</th>
-                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Staff Member Name: activate to sort column ascending" style="width: 356px;">Staff Member Name</th>
+                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Staff Member Name: activate to sort column ascending" style="width: 356px;">Faculty Name</th>
+                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Staff Member Name: activate to sort column ascending" style="width: 356px;">Group or Individual</th>
                     <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 322px;">Course Prefix</th>
                     <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 250px;">Course Number</th>
                     <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 322px;">Quarter</th>
                     <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 250px;">Year</th>
-                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 250px;">Completed</th>
+                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 250px;">Status</th>
                     <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 250px;">Date</th>
+                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 250px;">View TIP</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -78,44 +80,52 @@
                     <tr class="odd" role="row">
                         <td class="">M&S</td>
                         <td class="">John Doe</td>
+                        <td class="">individual</td>
                         <td class="">AD</td>
                         <td class="">IT125</td>
                         <td class="">Spring</td>
                         <td class="">2017</td>
-                        <td class="">75%</td>
+                        <td class="">completed</td>
                         <td class="">05-05-17</td>
+                        <td class=""></td>
                     </tr>
                     <tr class="even" role="row">
                         <td class="">M&S</td>
                         <td class="">Mary Moe</td>
+                        <td class="">group</td>
                         <td class="">AD</td>
                         <td class="">340</td>
                         <td class="">Spring</td>
                         <td class="">2017</td>
-                        <td class="">100%</td>
+                        <td class="">in-progress</td>
                         <td class="">04-02-17</td>
+                        <td class=""></td>
                     </tr>
                     <tr class="odd" role="row">
                         <td class="">M&S</td>
                         <td class="">Sally Smith</td>
+                        <td class="">individual</td>
                         <td class="">AD</td>
                         <td class="">410</td>
                         <td class="">Winter</td>
                         <td class="">2017</td>
-                        <td class="">25%</td>
+                        <td class="">completed</td>
                         <td class="">03-15-17</td>
+                        <td class=""></td>
                     </tr>
                     </tbody>
                     <tfoot>
                     <tr>
                     <th rowspan="1" colspan="1">Division</th>
-                    <th rowspan="1" colspan="1">Staff Member Name</th>
+                    <th rowspan="1" colspan="1">Faculty Name</th>
+                    <th rowspan="1" colspan="1">Group or Individual</th>
                     <th rowspan="1" colspan="1">Course Prefix</th>
                     <th rowspan="1" colspan="1">Course Number</th>
                     <th rowspan="1" colspan="1">Quarter</th>
                     <th rowspan="1" colspan="1">Year</th>
-                    <th rowspan="1" colspan="1">Completed</th>
+                    <th rowspan="1" colspan="1">Status</th>
                     <th rowspan="1" colspan="1">Date</th>
+                    <th rowspan="1" colspan="1">View TIP</th>
                     </tr>
                     </tfoot>
                     </table>
@@ -136,6 +146,7 @@
             </div>
             </div>
         </div>
+                                        
                         
 
 
