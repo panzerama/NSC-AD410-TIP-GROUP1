bef86efef44abdba8c34ab455f2b0575a0204d00
JD Panzer <jason.drew.panzer@gmail.com>
2017-05-18 15:07:21 +0000

    incrementally adding queries

--- prod/app/Http/Controllers/ReportsControllerDev.php
+++ prod/app/Http/Controllers/ReportsControllerDev.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\tip;
 
 class ReportsControllerDev extends Controller
 {
@@ -17,9 +18,11 @@ class ReportsControllerDev extends Controller
     public function summarytest()
     {
         //hardcode data (to do: replace with queries)
-        $count_submitted = DB::table('tips')->where('is_finished',1)->count();
-            $data = array('countSubmitted' => $count_submitted,
-                          'countInprogress' => '50',
+        $count_submitted = tip::where('is_finished', 1)->count();
+        //DB::table('tips')->where('is_finished',1)->count();
+        $count_in_progress = DB::table('tips')->where('is_finished',0)->count();
+        $data = array('countSubmitted' => $count_submitted,
+                          'countInprogress' => $count_in_progress,
                           'countNotstarted' => '230');  
         
         //return data to view
@@ -30,6 +33,7 @@ class ReportsControllerDev extends Controller
     public function tipsbymonthtest()
     {
         //hardcode data (to do: replace with queries)
+        //we need a datetime
         $month = array("July", "August", "September", "October", "November", "January", "February");
         $countByMthSubmitted = array(24, 14, 29, 10, 1, 30, 6);
         $countByMthInprogress = array(5, 10, 15, 17, 20, 4, 2);
@@ -45,6 +49,9 @@ class ReportsControllerDev extends Controller
     public function tipsbydivisiontest()
     {
         //hardcode data (to do: replace with queries)
+        $tips_by_division = DB::table('tips')
+                                ->join('divisions', 'tips.division_id', '=', 'divisions.division_id')
+                                ->orderBy('divisions');
         $division = array("AHSS", "BEIT", "BTS", "HHS", "LIB", "M&S");
         $countByDivisionSubmitted = array(65, 48, 29, 19, 13, 6);
         $countByDivisionInprogress = array(5, 10, 15, 7, 20, 42);
