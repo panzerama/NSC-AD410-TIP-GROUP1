c3c8dee2a7626e195524afc8bce8f5b114c682d2
ljs4x <luke.schwarz@gmail.com>
2017-05-19 19:52:18 +0000

    added a bunch of pauls work into LoginController

--- prod/app/Http/Controllers/Auth/LoginController.php
+++ prod/app/Http/Controllers/Auth/LoginController.php
@@ -5,15 +5,80 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+//config stuff:
+// these settings need to be changed to whatever our actual settings are.
+// redirectUri probably needs to be removed since we redirect depending on what
+// kind of user is coming into our app
+global  $config;
+$config = [
+    'canvasClientId' => '90000000000378',
+    'canvasClientSecret' => 'MTuYbZdJBuapeD0gKnbkA2mHaUMZYwPGHkQb6wvoX01sGcEksnQiBaQ80YyuVtEm',
+    'redirectUri'=> "http://www.icoolshow.net/nsc/tip",
+    'canvasInstanceUrl'=>'https://northseattle.test.instructure.com'
+];
+
+require_once "../vendor/autoload.php";
+
+use smtech\OAuth2\Client\Provider\CanvasLMS;
+use GuzzleHttp\Client;
+
+//start session
+session_start();
+
+// do we need these definitions?
+define('CODE', 'code');
+define('STATE', 'state');
+define('STATE_LOCAL', 'oauth2-state');
+// the above defined CODE gets used in the code of Pauls but do we need the rest?
+// or is this for something else entirely?
+
+// provider is our authentication provider yes/no?
+$provider = new CanvasLMS([
+    'clientId' => $config['canvasClientId'] ,
+    'clientSecret' => $config['canvasClientSecret'],
+    'purpose' => 'tip',
+    'redirectUri' => $config['redirectUri'],
+    'canvasInstanceUrl' => $config['canvasInstanceUrl']
+]);
+
+/* if we don't already have an authorization code, let's get one! */
+// here is where CODE was used. Do we even need this if statement? or do we 
+// want to just assume we have a code and if we don't than we kick them back to 
+// canvas if we don't?
+if (!isset($_GET[CODE])) {
+    $authorizationUrl = $provider->getAuthorizationUrl();
+    $_SESSION[STATE_LOCAL] = $provider->getState();
+    header("Location: $authorizationUrl");
+    exit;
+}
+else {
+    echo $_GET[CODE];
+    /* try to get an access token (using our existing code) */
+    $token = $provider->getAccessToken('authorization_code', [CODE => $_GET[CODE]]);
+    
+    //ownerDetails is just the canvas name and id associated with the token I think...
+    $ownerDetails = $provider->getResourceOwner($token);
+
+    // Use these details to create a new profile
+    $name = $ownerDetails->getName();
+    $uid= $ownerDetails->getId();
+    $domain = 'northseattle.test.instructure.com';
+
+    //use the id and the token to get the full profile from our test canvas setup
+    $profile_url = 'https://' . $domain . '/api/v1/users/' . $uid . '/profile?access_token=' . $token;
+    
+    $userFile = @file_get_contents($profile_url);
+    
+    // php built in JSON decode, 
+    $profile = json_decode($userFile);
+    
+    // now just get our email because that's all we need to check 
+    // against our faculty table
+    $email = $profile->primary_email;
+}
 class LoginController extends Controller
 {
     
-//TODO: PAULS SAMPLE CODE 
-// in the store() function(or whatever we made it)
-// copy in the config.php and index.php from Pauls work.
-// get the info for the config.php from here(?):  https://canvas.northseattle.edu/courses/1421135/pages/test-developer-api-key?module_item_id=22440853
-// attempt to authenticate(this is probably done fine in pauls work)
-
 //TODO: KIP - get the name and email
 
     //rasar: validateEmail(name, email)
