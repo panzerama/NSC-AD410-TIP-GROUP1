962279dbdf9d246ff28a7a820e16e0404e46f11d
Ilsa Coleman <icoleman00@gmail.com>
2017-05-29 03:01:34 +0000

    updating datatables

--- prod/config/app.php
+++ prod/config/app.php
@@ -169,7 +169,6 @@ return [
          * Package Service Providers...
          */
 
-        Yajra\Datatables\DatatablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Datatables' => Yajra\Datatables\Facades\Datatables::class,
 
     ],
 
