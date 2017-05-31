a12b75ce05285873f46316d5a045077bde2426bd
ljs4x <luke.schwarz@gmail.com>
2017-05-31 20:57:26 +0000

    fixed typo

--- prod/app/Http/Controllers/AccountController.php
+++ prod/app/Http/Controllers/AccountController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\LoginController.php;
+use App\Http\Controllers\LoginController;
 
 class AccountController extends Controller
 {
