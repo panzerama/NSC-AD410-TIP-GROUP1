ba908bc4e3b273043fcda32e2906cd542e9fb989
JD Panzer <jason.drew.panzer@gmail.com>
2017-06-04 06:05:08 +0000

    Updated report and admin nav

--- prod/storage/framework/views/e9ba545c8bf9408251e8a13d35a859b247ad664c.php
+++ prod/storage/framework/views/e9ba545c8bf9408251e8a13d35a859b247ad664c.php
@@ -6,7 +6,6 @@
                         <span class="clear">
                             <span class="block m-t-xs">
                                 <img src="/images/nsc_logo_t.png" height="64" width="64">
-                                &nbsp;&nbsp;&nbsp;
                                 <strong class="font-bold">Michael Fraser</strong>
                             </span> 
                         </span>
