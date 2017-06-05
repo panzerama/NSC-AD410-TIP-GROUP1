466a4f4bedf8c4f7554042d668a83b74113ad936
ljs4x <luke.schwarz@gmail.com>
2017-06-05 17:16:42 +0000

    small config change

--- prod/app/Http/Controllers/Auth/config.php
+++ prod/app/Http/Controllers/Auth/config.php
@@ -3,7 +3,7 @@ global  $config;
 $config = [
     'canvasClientId' => '1358940000000000001',
     'canvasClientSecret' => 'WSAAgWP3kHAFOgbH5q4FpoIF8syLcfjrsTghd1Ybx7NYhcdGNYn9hpGY1VRakHdB',
-    'redirectUri'=> 'https://dev.mycollege.tips/auth/',
+    'redirectUri'=> 'https://dev.mycollege.tips',
     'canvasInstanceUrl'=>'https://north-seattle-college.acme.instructure.com'
 ];
 ?>
