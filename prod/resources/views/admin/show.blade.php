ff97589a7d290ef98fc680c02bb444d8e64c872c
mitch.thompson <thomitchell@gmail.com>
2017-06-06 21:37:47 +0000

    Adds tabs to UI of faculty list view.

--- prod/resources/views/admin/show.blade.php
+++ prod/resources/views/admin/show.blade.php
@@ -8,63 +8,63 @@
     <div class="row">
         <div class="col-lg-12">
             <h2>Faculty List</h2>
-            <br><br>
-            <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                  <h5>Active Faculty</h5>
-                </div>
-                <div class="ibox-content">
-                        <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Change Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($faculty as $Singlefaculty)
-                                @if(!$Singlefaculty->is_admin && $Singlefaculty->is_active)
-                                    <tr>
-                                        <td>{{ $Singlefaculty->faculty_name }}</td>
-                                        <td>{{ $Singlefaculty->email }}</td>
-                                        <td><a href="{{ url('/admin/update') }}/{{ $Singlefaculty->faculty_id }}/active" class="btn btn-primary">Make Inactive</button></td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div><!-- ibox -content -->
-            </div><!-- ibox -->
-            
-            <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                  <h5>Inactive Faculty</h5>
-                </div>
-                <div class="ibox-content">
-                        <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Change Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($faculty as $Singlefaculty)
-                                @if(!$Singlefaculty->is_admin && !$Singlefaculty->is_active)
-                                    <tr>
-                                        <td>{{ $Singlefaculty->faculty_name }}</td>
-                                        <td>{{ $Singlefaculty->email }}</td>
-                                        <td><a href="{{ url('/admin/update') }}/{{ $Singlefaculty->faculty_id }}/inactive" class="btn btn-primary">Make Active</button></td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div><!-- ibox -content -->
-            </div><!-- ibox -->
-            
+            <br>
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#active">Active Faculty</a></li>
+              <li><a data-toggle="tab" href="#inactive">Inactive Faculty</a></li>
+            </ul>
+
+            <div class="tab-content">
+                <div id="active" class="ibox float-e-margins tab-pane fade in active">
+                    <div class="ibox-content">
+                            <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Change Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($faculty as $Singlefaculty)
+                                    @if(!$Singlefaculty->is_admin && $Singlefaculty->is_active)
+                                        <tr>
+                                            <td>{{ $Singlefaculty->faculty_name }}</td>
+                                            <td>{{ $Singlefaculty->email }}</td>
+                                            <td><a href="{{ url('/admin/update') }}/{{ $Singlefaculty->faculty_id }}/active" class="btn btn-primary">Make Inactive</button></td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div><!-- ibox -content -->
+                </div><!-- ibox -->
+                
+                 <div id="inactive" class="ibox float-e-margins tab-pane fade in inactive">
+                    <div class="ibox-content">
+                            <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Change Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($faculty as $Singlefaculty)
+                                    @if(!$Singlefaculty->is_admin && !$Singlefaculty->is_active)
+                                        <tr>
+                                            <td>{{ $Singlefaculty->faculty_name }}</td>
+                                            <td>{{ $Singlefaculty->email }}</td>
+                                            <td><a href="{{ url('/admin/update') }}/{{ $Singlefaculty->faculty_id }}/inactive" class="btn btn-primary">Make Active</button></td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div><!-- ibox -content -->
+                </div><!-- ibox -->
+            </div><!-- tab-content-->
         </div><!-- col-lg-12 -->
     </div><!-- row -->
 </div><!-- wrapper -->
