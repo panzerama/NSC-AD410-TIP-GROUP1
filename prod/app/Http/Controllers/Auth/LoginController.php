5696e4e664d0fa3508a28c1b246e63b9c2b7ee49
ljs4x <luke.schwarz@gmail.com>
2017-06-07 02:59:55 +0000

    added destroy() to logincontroller

--- prod/app/Http/Controllers/Auth/LoginController.php
+++ prod/app/Http/Controllers/Auth/LoginController.php
@@ -118,5 +118,10 @@ class LoginController extends Controller
     }
     
     // destroy() go here:
+    public function destroy() {
+        Auth::logout();
+        // redirect to canvas homepage or to our index page?
+        // return redirect('canvas.northseattle.edu')
+    }
 
 }
