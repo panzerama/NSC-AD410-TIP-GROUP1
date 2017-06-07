5829b770a195587035a4260898a2bae5e1f89d2c
Ilsa Coleman <icoleman00@gmail.com>
2017-06-07 02:18:02 +0000

    update colors

--- prod/resources/views/reports/primary-elo-added.blade.php
+++ prod/resources/views/reports/primary-elo-added.blade.php
@@ -26,8 +26,8 @@
     labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
     datasets: [
         {
-            backgroundColor: 'rgba(0,142,226,0.5)',
-            borderColor: "rgba(0,142,226,0.7)",
+            backgroundColor: 'rgba(0,142,226,1)',
+            borderColor: "rgba(0,142,226,1)",
             pointBackgroundColor: "rgba(0,142,226,1)",
             pointBorderColor: "#fff",
             data: [28, 48, 40, 19, 86, 27, 15, 65, 21, 47, 32, 8]
