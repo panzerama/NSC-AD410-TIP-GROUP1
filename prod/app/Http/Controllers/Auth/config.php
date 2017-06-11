90d251c121005f46f610d0a4a969f1eefd9b9225
MFraser <mfraser@infiniteops.io>
2017-06-11 11:56:22 -0700

    Update to LoginController

--- prod/app/Http/Controllers/Auth/config.php
+++ prod/app/Http/Controllers/Auth/config.php
@@ -4,7 +4,7 @@ $config = [
     'canvasClientId' => '90000000000384',
     'canvasClientSecret' => 'jJEgbfvMKyaYO10fXFG6ISyNGKGyzNlszuA37EtAr0C6Gdu52SOBBIxVjEtyO6R0',
     'redirectUri'=> 'https://dev.mycollege.tips',
-    'canvasInstanceUrl'=>' https://northseattle.test.instructure.com'
+    'canvasInstanceUrl'=>'https://northseattle.test.instructure.com'
 ];
 ?>
 
