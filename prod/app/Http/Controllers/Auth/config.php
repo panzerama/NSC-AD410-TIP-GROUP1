f0a3426ae5f40040f0c2f9dbaedfdde1d8af1ff9
Michael Fraser <itascode@outlook.com>
2017-06-11 13:35:44 -0500

    Update to oauth2 testing

--- prod/app/Http/Controllers/Auth/config.php
+++ prod/app/Http/Controllers/Auth/config.php
@@ -1,9 +1,10 @@
 <?php
 global  $config;
 $config = [
-    'canvasClientId' => '90000000000384',
-    'canvasClientSecret' => 'jJEgbfvMKyaYO10fXFG6ISyNGKGyzNlszuA37EtAr0C6Gdu52SOBBIxVjEtyO6R0B',
+    'canvasClientId' => '1378360000000000001',
+    'canvasClientSecret' => 'txXp9cBfoJpOAGmivII0f87bxUi7db6K0l86LdpfZUZblIMVAh9PXia1DV8NskJM',
     'redirectUri'=> 'https://dev.mycollege.tips',
-    'canvasInstanceUrl'=>'https://northseattle.test.instructure.com'
+    'canvasInstanceUrl'=>'https://north-seattle-college.acme.instructure.com'
 ];
 ?>
+
