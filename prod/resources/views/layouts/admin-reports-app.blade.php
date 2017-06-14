3367709ebe43acad3e4979c9f5bc417acd57ebc3
Ilsa Coleman <icoleman00@gmail.com>
2017-06-14 21:50:45 +0000

    removes filters from nav bar in reports

--- prod/resources/views/layouts/admin-reports-app.blade.php
+++ prod/resources/views/layouts/admin-reports-app.blade.php
@@ -20,7 +20,7 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-        @include('layouts.nav-admin-reports')
+        @include('layouts.nav-admin')
 
         <!-- Page wraper -->
         <div id="page-wrapper" class="gray-bg">
