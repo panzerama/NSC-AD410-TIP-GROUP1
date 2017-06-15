e1c9c8985fbbb1bbb9e697e219ff10600aa83492
JD Panzer <jason.drew.panzer@gmail.com>
2017-06-15 07:25:46 +0000

    Update LoginController

--- prod/routes/web.php
+++ prod/routes/web.php
@@ -59,7 +59,7 @@ Route::get('/logout', 'Auth\LoginController@destroy')->name('logout'); // Log Ou
  Route::get('/admin','AdminController@index'); // Splash Page for Admin Functions
  Route::get('/admin/create', 'AdminController@create');
  Route::post('/admin/show','AdminController@store')->name('adminStore'); // Submit and Store New Admin Form
- Route::get('/admin/show','AdminController@show')->middleware('auth:web')->middleware('auth.admin:web'); //show faculty list // Create New Admin Form
+ Route::get('/admin/show','AdminController@show'); //->middleware('auth:web','auth.admin:web'); //show faculty list // Create New Admin Form
  Route::get('/admin/update/{id}/{status}', 'AdminController@update'); //Change User Status
  
  
